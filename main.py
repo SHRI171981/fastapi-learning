36f085dc00a9aa5b058d7218bec7d566b110108b
SHRI171981 <shrinidhikumar.2003@gmail.com>
2026-02-24 00:38:22 +0530

    video2: introduction

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/about")
+def about():
+    return {"message": "This is a simple FastAPI application."}
+
+# uvicorn main:app --reload
